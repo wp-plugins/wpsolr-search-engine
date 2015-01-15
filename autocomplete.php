70b9707affcf3ef1a00e0fc93023cec87ab4b589
wpsolr <wpsolr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-15 08:05:24 +0000

    Fix autocomplete for cloud hosting
    git-svn-id: https://plugins.svn.wordpress.org/wpsolr-search-engine/trunk@1068397 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- autocomplete.php
+++ autocomplete.php
@@ -9,26 +9,58 @@ function wdm_return_solr_rows() {
 
 		$input = $_POST['word'];
 
-		if ( $_SESSION['wdm-host'] == '' ) {
-			$solr_options         = get_option( 'wdm_solr_conf_data' );
-			$_SESSION['wdm-host'] = $solr_options['solr_host'];
-			$_SESSION['wdm-port'] = $solr_options['solr_port'];
-			$_SESSION['wdm-path'] = $solr_options['solr_path'];
-		}
-		$host  = $_SESSION['wdm-host'];
-		$port  = $_SESSION['wdm-port'];
-		$spath = $_SESSION['wdm-path'];
+		$solr_options = get_option( 'wdm_solr_conf_data' );
+		$host_type    = $solr_options['host_type'];
 
-		$config = array(
-			"endpoint" =>
-				array(
-					"localhost" => array(
-						"host" => $host,
-						"port" => $port,
-						"path" => $spath
+		$postix = '_goto';
+		if ( $host_type == 'self_hosted' ) {
+			$postix = '';
+		}
+
+		if ( $_SESSION[ $host_type ] == '' ) {
+			$_SESSION[ 'wdm-host' . $postix ]  = $solr_options[ 'solr_host' . $postix ];
+			$_SESSION[ 'wdm-port' . $postix ]  = $solr_options[ 'solr_port' . $postix ];
+			$_SESSION[ 'wdm-path' . $postix ]  = $solr_options[ 'solr_path' . $postix ];
+			$_SESSION[ 'wdm-user' . $postix ]  = $solr_options[ 'solr_key' . $postix ];
+			$_SESSION[ 'wdm-pwd' . $postix ]   = $solr_options[ 'solr_secret' . $postix ];
+			$_SESSION[ 'wdm-proto' . $postix ] = $solr_options[ 'solr_protocol' . $postix ];
+		}
+
+		$host     = $_SESSION[ 'wdm-host' . $postix ];
+		$port     = $_SESSION[ 'wdm-port' . $postix ];
+		$spath    = $_SESSION[ 'wdm-path' . $postix ];
+		$username = $_SESSION[ 'wdm-user' . $postix ];
+		$password = $_SESSION[ 'wdm-pwd' . $postix ];
+		$protocol = $_SESSION[ 'wdm-proto' . $postix ];
+
+		if ( $host_type == 'self_hosted' ) {
+			$config = array(
+				"endpoint" =>
+					array(
+						"localhost" => array(
+							"host" => $host,
+							"port" => $port,
+							"path" => $spath
+						)
 					)
-				)
-		);
+			);
+		} else {
+			$config = array(
+				"endpoint" =>
+					array(
+						"localhost1" => array(
+							"scheme"   => $protocol,
+							"host"     => $host,
+							"port"     => $port,
+							"path"     => $spath,
+							'username' => $username,
+							'password' => $password
+						)
+					)
+			);
+		}
+
+
 		require( 'vendor/autoload.php' );
 		$client = new Solarium\Client( $config );
 		$input  = strtolower( $input );
@@ -61,3 +93,4 @@ function wdm_return_solr_rows() {
 
 add_action( 'wp_ajax_wdm_return_solr_rows', 'wdm_return_solr_rows' );
 add_action( 'wp_ajax_nopriv_wdm_return_solr_rows', 'wdm_return_solr_rows' );
+
